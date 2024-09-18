d17b659532031caba5443f334919f71bbe9ae159
gloginov <georgeloginov90@gmail.com>
2024-09-18 23:26:40 +0500

    [SOK-12] +base pages +separated layouts for private/public pages +scss +base promo page +ui component Button +fonts

---
+++ packages/client/src/app/App.tsx
@@ -0,0 +1,78 @@
+// import { useEffect } from 'react'
+
+import '@/scss/styles.scss'
+import '@/app/App.scss'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import RootLayout from "@/layouts/root-layout";
+import PrivateLayout from "@/layouts/private-layout";
+import AuthLayout from "@/layouts/auth-layout";
+import PublicLayout from "@/layouts/public-layout";
+import { Main } from "@/pages/Main/Main";
+import { SignIn } from "@/pages/SignIn/SignIn";
+import { SignUp } from "@/pages/SignUp/SignUp";
+import { Game } from "@/pages/Game/Game";
+import { Forum } from "@/pages/Forum/Forum";
+import { Error } from "@/pages/Error/Error";
+
+const routerConfig = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        element: <PublicLayout />,
+        children: [
+          {
+            index: true,
+            element: <Main />
+          }
+        ]
+      },
+      {
+        element: <PrivateLayout />,
+        children: [
+          {
+            path: '/game',
+            element: <Game />
+          },
+          {
+            path: '/forum',
+            element: <Forum />
+          }
+        ]
+      },
+      {
+        element: <AuthLayout />,
+        children: [
+          {
+            path: '/sign-in',
+            element: <SignIn />
+          },
+          {
+            path: '/sign-up',
+            element: <SignUp />
+          }
+        ]
+      }
+    ]
+  }
+])
+
+function App() {
+  // useEffect(() => {
+  //   const fetchServerData = async () => {
+  //     const url = `http://localhost:${__SERVER_PORT__}`
+  //     const response = await fetch(url)
+  //     const data = await response.json()
+  //     console.log(data)
+  //   }
+  //
+  //   fetchServerData()
+  // }, [])
+  //
+  return (
+    <RouterProvider router={routerConfig} />
+  )
+}
+
+export default App
---
+++ packages/client/src/components/ui/Button/Button.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import './Button.scss'
+
+export const Button = (props: { text: string, className: string, useFixWidth: boolean, href?: string }) => {
+  const { text, className, useFixWidth = false, href = '/' } = props;
+  return (
+    <Link to={href} className={`custom-button ${className} ${useFixWidth ? 'custom-button_fix-width' : ''}`}>
+      <span>{text}</span>
+    </Link>
+  )
+}
