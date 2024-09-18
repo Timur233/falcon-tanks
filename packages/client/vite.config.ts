a1994329277f8754ce20a0ba22ee00b08bdc7236
gloginov <georgeloginov90@gmail.com>
2024-09-18 21:27:31 +0500

    [init_docker] *add container for dev development and hot reload

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,14 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 dotenv.config()
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: Number(process.env.CLIENT_PORT) || 3000,
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true
+    }
   },
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
   plugins: [react()],
 })
