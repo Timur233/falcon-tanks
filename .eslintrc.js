3c71d734b0280f594a7bbf544ebe95c01aa1837c
Iskandarov Timur <timur.iskandarov96@gmail.com>
2024-09-27 12:35:51 +0500

    Добавил поддержку jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,14 +7,22 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
+    sourceType: 'module', 
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/ban-ts-comment': 1,
+    'quotes': ['error', 'single']
   },
 }
