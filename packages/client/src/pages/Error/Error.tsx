dac3864661bd08dd0547f1bbba03b90e422dccf1
Iskandarov Timur <timur.iskandarov96@gmail.com>
2024-09-27 11:37:05 +0500

    Error Page. Обернул станицу в layout Сам лайоут написал внутри страницы.

--- packages/client/src/pages/Error/Error.tsx
+++ packages/client/src/pages/Error/Error.tsx
@@ -1,6 +1,7 @@
 import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
 import DestroyedTanksImage from '../../assets/images/destroyed-tanks.png'
 import './Error.scss'
+import { Header } from '@/components/common/Header/Header'
 
 export const Error = () => {
   const error = useRouteError()
@@ -16,16 +17,21 @@ export const Error = () => {
   }
 
   return (
-    <div className={'error-page'}>
-      <div className={'error-page__info'}>
-        <span>{status || 'Oops!'}</span>
-        <h1>{statusText}</h1>
+    <div className="error-page-layout">
+      <Header className='error-page-layout__header'/>
+      <div className="error-page-layout__body">
+        <div className={'error-page'}>
+          <div className={'error-page__info'}>
+            <span>{status || 'Oops!'}</span>
+            <h1>{statusText}</h1>
+          </div>
+          <img
+            className={'error-page__destroyed-tanks'}
+            src={DestroyedTanksImage}
+            alt={'Destroyed tanks'}
+          />
+        </div>
       </div>
-      <img
-        className={'error-page__destroyed-tanks'}
-        src={DestroyedTanksImage}
-        alt={'Destroyed tanks'}
-      />
     </div>
   )
 }
