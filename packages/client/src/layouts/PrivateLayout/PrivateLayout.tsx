b5deac986b8d7671afe2cef6aa7ad6069ccb0d59
k0ndratov <93945565+k0ndratov@users.noreply.github.com>
2024-09-28 18:09:35 +0500

    [SOK-20] leaderboard page

--- packages/client/src/layouts/PrivateLayout/PrivateLayout.tsx
+++ packages/client/src/layouts/PrivateLayout/PrivateLayout.tsx
@@ -1,5 +1,13 @@
+import { Header } from '@/components/common/Header/Header'
 import { Outlet } from 'react-router-dom'
 
 export default function PrivateLayout() {
-  return <Outlet />
+  return (
+    <div className="private-layout">
+      <Header className="private-layout__header"></Header>
+      <main className="private-layout__body">
+        <Outlet />
+      </main>
+    </div>
+  )
 }
--- packages/client/src/pages/Leaderboard/Leaderboard.tsx
+++ packages/client/src/pages/Leaderboard/Leaderboard.tsx
@@ -1,3 +1,51 @@
+import PlayerAvatar from '@/assets/images/player-avatar.png'
+import StarColor from '@/assets/images/stars/star-color.png'
+import PromoLogo from '@/assets/images/svg/FT-promo.svg'
+import { PageTitle } from '@/components/ui/PageTitle/PageTitle'
+import './Leaderboard.scss'
+
+const RECORD_ITEMS_COUNT = 6
+
 export const Leaderboard = () => {
-  return <>Тут будет доска с результатами лучших игроков и местом в рейтинге</>
+  return (
+    <div className="container leaderboard-page">
+      <img className="leaderboard-page__logo" src={PromoLogo} alt="Tank wars" />
+      <PageTitle
+        text={'Рекорды'}
+        tagName="h1"
+        className="leaderboard-page__title"></PageTitle>
+      <div className="records">
+        {Array.from({ length: RECORD_ITEMS_COUNT }).map((_, index) => {
+          return (
+            <div key={index} className="records__item-wrapper">
+              <div className="records__item record-item">
+                <div className="record-item__user-info user-info">
+                  <img
+                    className="user-info__avatar"
+                    src={PlayerAvatar}
+                    alt="Avatar"
+                  />
+                  <div className="user-info__login">
+                    <span className="record-item__title">Логин</span>
+                    <h2 className="record-item__value">Gamer 1235r124</h2>
+                  </div>
+                </div>
+                <div className="record-item__user-result user-result">
+                  <div className="user-result__score">
+                    <span className="record-item__title">Рекорд</span>
+                    <h2 className="record-item__value">999</h2>
+                  </div>
+                  <img
+                    className="user-result__star"
+                    src={StarColor}
+                    alt="Медаль"
+                  />
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
