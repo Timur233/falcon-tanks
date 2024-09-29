143dcff7696519ae23cf342ca5995e96ad340abd
Iskandarov Timur <timur.iskandarov96@gmail.com>
2024-09-29 05:20:33 +0500

    Сврерстал страницу игры и окно начала игры

--- packages/client/src/pages/Game/components/Arrows/Arrows.tsx
+++ packages/client/src/pages/Game/components/Arrows/Arrows.tsx
@@ -1,107 +1,58 @@
-import { useEffect, useState } from 'react'
-import { ButtonMid } from '../buttons/ButtonMid/ButtonMid'
+import { ControllBtn } from '../ControllBtn/ControllBtn'
 import './Arrows.scss'
+import { Icon } from '@/components/ui/Icon/Icon'
 
-export const Arrows = () => {
-  const [activeButtons, setActiveButtons] = useState({
-    upButton: false,
-    downButton: false,
-    leftButton: false,
-    rightButton: false,
-  })
+type ArrowsPropsType = {
+  className?: string
+  buttonsState: {
+    upButton: boolean
+    downButton: boolean
+    leftButton: boolean
+    rightButton: boolean
+    fireButton: boolean
+  },
+  clickHandler: () => void,
+}
 
-  const toggleButton = (buttonName: string, state: boolean) => {
-    setActiveButtons({ ...activeButtons, [buttonName]: state })
-  }
-
-  const handleKey = (event: KeyboardEvent) => {
-    console.log(activeButtons)
-
-    switch (event.key) {
-      case 'ArrowUp':
-        toggleButton('upButton', event.type === 'keydown' ? true : false)
-        break
-      case 'ArrowDown':
-        toggleButton('downButton', event.type === 'keydown' ? true : false)
-        break
-      case 'ArrowLeft':
-        toggleButton('leftButton', event.type === 'keydown' ? true : false)
-        break
-      case 'ArrowRight':
-        toggleButton('rightButton', event.type === 'keydown' ? true : false)
-        break
-      default:
-        break
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleKey)
-    window.addEventListener('keyup', handleKey)
-
-    return () => {
-      window.removeEventListener('keydown', handleKey)
-      window.removeEventListener('keyup', handleKey)
-    }
-  })
+export const Arrows = (props: ArrowsPropsType) => {
+  const { className, buttonsState, clickHandler } = props
 
   return (
-    <div className="controll-arrows">
+    <div className={`controll-arrows ${className}`}>
       <div className="controll-arrows__horizontal horizontal-arrows">
-        <ButtonMid
-          buttonName="leftButton"
-          isPressed={activeButtons.leftButton}
-          toggleButton={toggleButton}>
-          <svg
-            width="33"
-            height="33"
-            viewBox="0 0 33 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M30 0L30 33L3.5 16.5L30 0Z" fill="white" />
-          </svg>
-        </ButtonMid>
-        <ButtonMid
-          buttonName="rightButton"
-          isPressed={activeButtons.rightButton}
-          toggleButton={toggleButton}>
-          <svg
-            width="33"
-            height="33"
-            viewBox="0 0 33 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 33V0L29.5 16.5L3 33Z" fill="white" />
-          </svg>
-        </ButtonMid>
+        <ControllBtn
+          className={`controll-btn_big ${
+            buttonsState.leftButton ? 'controll-btn_active' : ''
+          }`}
+          onClick={clickHandler}>
+          <Icon id="arrow-left" width={33} height={33}></Icon>
+        </ControllBtn>
+
+        <ControllBtn
+          className={`controll-btn_big ${
+            buttonsState.rightButton ? 'controll-btn_active' : ''
+          }`}
+          onClick={clickHandler}>
+          <Icon id="arrow-right" width={33} height={33}></Icon>
+        </ControllBtn>
       </div>
+
       <div className="controll-arrows__vertical vertical-arrows">
-        <ButtonMid
-          buttonName="leftButton"
-          isPressed={activeButtons.leftButton}
-          toggleButton={toggleButton}>
-          <svg
-            width="33"
-            height="33"
-            viewBox="0 0 33 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M30 0L30 33L3.5 16.5L30 0Z" fill="white" />
-          </svg>
-        </ButtonMid>
-        <ButtonMid
-          buttonName="rightButton"
-          isPressed={activeButtons.rightButton}
-          toggleButton={toggleButton}>
-          <svg
-            width="33"
-            height="33"
-            viewBox="0 0 33 33"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 33V0L29.5 16.5L3 33Z" fill="white" />
-          </svg>
-        </ButtonMid>
+        <ControllBtn
+          className={`controll-btn_big ${
+            buttonsState.upButton ? 'controll-btn_active' : ''
+          }`}
+          onClick={clickHandler}>
+          <Icon id="arrow-up" width={33} height={33}></Icon>
+        </ControllBtn>
+
+        <ControllBtn
+          className={`controll-btn_big ${
+            buttonsState.downButton ? 'controll-btn_active' : ''
+          }`}
+          onClick={clickHandler}>
+          <Icon id="arrow-down" width={33} height={33}></Icon>
+        </ControllBtn>
       </div>
     </div>
   )
