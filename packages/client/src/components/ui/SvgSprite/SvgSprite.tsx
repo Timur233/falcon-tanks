0fbd8c92c2d4f6aa952cc7b52cc5f75be1802a9a
Iskandarov Timur <timur.iskandarov96@gmail.com>
2024-10-01 16:42:43 +0500

    Добавил состояние для загрузки спрайта иконок

--- packages/client/src/components/ui/SvgSprite/SvgSprite.tsx
+++ packages/client/src/components/ui/SvgSprite/SvgSprite.tsx
@@ -1,22 +1,27 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export const SvgSprite = () => {
+  const [isLoaded, setIsLoaded] = useState(false)
+
   useEffect(() => {
-    fetch('/sprite.symbol.svg')
-      .then(res => res.text())
-      .then(svgContent => {
-        const sprite = document.querySelector('#svg-sprite')
+    if (isLoaded === false) {
+      fetch('/sprite.symbol.svg')
+        .then(res => res.text())
+        .then(svgContent => {
+          const sprite = document.querySelector('#svg-sprite')
 
-        if (!sprite) {
-          const div = document.createElement('div')
+          if (!sprite) {
+            const div = document.createElement('div')
 
-          div.id = 'svg-sprite'
-          div.style.display = 'none'
-          div.innerHTML = svgContent
+            div.id = 'svg-sprite'
+            div.style.display = 'none'
+            div.innerHTML = svgContent
 
-          document.body.appendChild(div)
-        }
-      })
+            document.body.appendChild(div)
+            setIsLoaded(true)
+          }
+        })
+    }
   }, [])
 
   return null
