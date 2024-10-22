403ad231279d708142f72469da0bc950f9c0dd22
Daniil <77277774+shamemask@users.noreply.github.com>
2024-10-22 20:55:30 +0300

    feat: добавил fps в игровой цикл

--- packages/client/src/components/Game/gameLoop.tsx
+++ packages/client/src/components/Game/gameLoop.tsx
@@ -24,6 +24,10 @@ import { updateBullets } from '@/components/Game/bullet'
 import { handleBulletObstacleCollisions } from '@/components/Game/obstacle'
 import { createBangEffect, initEffects } from './effects'
 
+const MAX_FPS = 60
+const FRAME_DURATION = 1000 / MAX_FPS
+let lastFrameTime = 0
+
 /**
  * Основной игровой цикл, который обновляет состояние игры и перерисовывает экран каждый кадр.
  * @param context - Контекст рисования для Canvas.
@@ -51,6 +55,30 @@ export const gameLoop = (
   handleGameOver: () => void,
   handleEnemyKilled: () => void
 ) => {
+  const now = performance.now()
+  const deltaTime = now - lastFrameTime
+
+  if (deltaTime < FRAME_DURATION) {
+    requestAnimationFrame(() =>
+      gameLoop(
+        context,
+        canvasRef,
+        playerRef,
+        enemiesRef,
+        bulletsRef,
+        obstaclesRef,
+        effectsRef,
+        livesRef,
+        handleDeath,
+        handleGameOver,
+        handleEnemyKilled
+      )
+    )
+    return
+  }
+
+  lastFrameTime = now
+
   clearCanvas(context)
 
   // Обновление позиций врагов
