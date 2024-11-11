7afc03a1040e83b29eea5c5f68a2794a3ad0b240
Daniil <77277774+shamemask@users.noreply.github.com>
2024-11-11 20:01:41 +0300

    fix: вынес подсчеты в отдельные функции sok-62

--- packages/client/src/components/Game/sound/surround.tsx
+++ packages/client/src/components/Game/sound/surround.tsx
@@ -1,7 +1,21 @@
 import { AbstractEntity } from '@/components/Game/gameTypes'
 import { MAX_DISTANCE } from '@/components/Game/constants'
 
-// Функция для проигрывания звука с учётом центра карты
+const distanceMath = (x1: number, y1: number, x2: number, y2: number) => {
+  const dx = x1 - x2
+  const dy = y1 - y2
+  return Math.hypot(dx, dy)
+}
+
+const mathVolumeByDistance = (distance: number) => {
+  return Math.max(0, 1 - distance / MAX_DISTANCE)
+}
+
+const mathPanByDistance = (distance: number) => {
+  // Рассчитываем панорамирование (-1 — левый край, 1 — правый)
+  return Math.max(-1, Math.min(1, distance / MAX_DISTANCE))
+}
+
 export const playShotSound = (
   sound: Howl,
   sourcePosition: AbstractEntity,
@@ -11,18 +25,15 @@ export const playShotSound = (
   const centerX = canvasWidth / 2
   const centerY = canvasHeight / 2
 
-  const distance = Math.hypot(
-    sourcePosition.x - centerX,
-    sourcePosition.y - centerY
+  const distance = distanceMath(
+    centerX,
+    centerY,
+    sourcePosition.x,
+    sourcePosition.y
   )
 
-  const volume = Math.max(0, 1 - distance / MAX_DISTANCE)
-
-  // Рассчитываем панорамирование (-1 — левый край, 1 — правый)
-  const pan = Math.max(
-    -1,
-    Math.min(1, (sourcePosition.x - centerX) / (canvasWidth / 2))
-  )
+  const volume = mathVolumeByDistance(distance)
+  const pan = mathPanByDistance(distance)
 
   sound.volume(volume) // Устанавливаем громкость
   sound.stereo(pan) // Устанавливаем панорамирование
